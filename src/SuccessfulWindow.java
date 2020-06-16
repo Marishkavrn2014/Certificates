8cf16b30ea5fb108af1a7256193cca9071eb23bb
Marina Shadrina <marishka_vrn@list.ru>
2020-06-16 18:37:17 +0300

    Project for printing certificates for language school

---
+++ src/SuccessfulWindow.java
@@ -0,0 +1,34 @@
+//this window requires the user to verify that the certificate is created
+import javax.swing.*;
+import java.awt.event.*;
+
+public class SuccessfulWindow extends JDialog {
+    private JPanel contentPane;
+    private JButton okButton;
+    private JLabel message;
+
+    public SuccessfulWindow() {
+        setContentPane(contentPane);
+        setVisible(true);
+        setSize(200, 200);
+        setModal(true);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        okButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                dispose();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+            SwingUtilities.invokeLater(
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            new SuccessfulWindow();
+                        }
+                    }
+            );
+    }
+}
